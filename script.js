cdaa75783597592291702c73b5784ac8ba72c8b4
emoltz <es5541@nyu.edu>
2022-06-13 16:16:19 -0400

    geolocation

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+
+//_____________________________________________ELEMENTS
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -11,8 +13,14 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-navigator.geolocation.getCurrentPosition(function(position){
-    console.log(position);
-}, function(){
-    alert('Could not get GPS position')
-})
+navigator.geolocation.getCurrentPosition(
+    function (position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        console.log(latitude, longitude);
+
+    },
+    function () {
+        alert('Could not get GPS position');
+    })
