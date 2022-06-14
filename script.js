7de6d3b2a50439666e27caccd13284404a6b05c4
emoltz <es5541@nyu.edu>
2022-06-14 10:35:28 -0400

    your location working

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // prettier-ignore
+
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 
@@ -12,18 +13,22 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+const zoomLevel = 15;
+
 
 navigator.geolocation.getCurrentPosition(function (position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-    const map = L.map('map').setView([latitude, longitude], 13);
+    const URL = `https://www.google.com/maps/@${latitude},${longitude}`;
+    const coords = [latitude, longitude];
+    const map = L.map('map').setView(coords, zoomLevel);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([latitude, longitude]).addTo(map)
+    L.marker(coords).addTo(map)
         .bindPopup('Your location!')
         .openPopup();
 
