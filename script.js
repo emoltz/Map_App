95c147f5a8186062ac62bd3a06c9cf857d4134e8
emoltz <es5541@nyu.edu>
2022-06-14 11:30:56 -0400

    click listeners with forms

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 const zoomLevel = 15;
 
+let map, mapEvent;
+
+function clickError(){
+    alert("Could not get position");
+}
+
 
 navigator.geolocation.getCurrentPosition(function (position) {
     const latitude = position.coords.latitude;
@@ -22,16 +28,46 @@ navigator.geolocation.getCurrentPosition(function (position) {
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
     const URL = `https://www.google.com/maps/@${latitude},${longitude}`;
     const coords = [latitude, longitude];
-    const map = L.map('map').setView(coords, zoomLevel);
+    map = L.map('map').setView(coords, zoomLevel);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker(coords).addTo(map)
+    L.marker(coords)
+        .addTo(map)
         .bindPopup('Your location!')
         .openPopup();
+    map.on('click', function(mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+        // createPin();
+
+    },clickError)
 
 }, function () {
     alert('Could not get GPS position');
+})
+
+function createPin(){
+    const {lat, lng} = mapEvent.latlng;
+    const currCoords = [lat, lng];
+    L.marker(currCoords).addTo(map).bindPopup(L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        // closeOnClick: false,
+        className: `normal-walk-popup`,
+    }))
+        .setPopupContent('New Popup')
+        .openPopup();
+
+}
+
+form.addEventListener('submit', function(e){
+    //display marker
+    e.preventDefault();
+    createPin();
+
 })
