81cf4adc8bea53dbd9817b3bc7f2d46d2bb97328
emoltz <es5541@nyu.edu>
2022-06-14 10:14:55 -0400

    your location working

--- script.js
+++ script.js
@@ -16,16 +16,15 @@ const inputElevation = document.querySelector('.form__input--elevation');
 navigator.geolocation.getCurrentPosition(function (position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-    const map = L.map('map').setView([51.505, -0.09], 13);
+    const map = L.map('map').setView([latitude, longitude], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup('Your location!')
         .openPopup();
 
 }, function () {
