bef5bab70bc0952a69ac6bde4fddaa719c08e26c
emoltz <es5541@nyu.edu>
2022-06-19 17:30:20 -0400

    bloggo update

--- README.md
+++ README.md
@@ -3,5 +3,11 @@
 
 Simple geo/map app for javascript practice. https://emoltz.github.io/WalkPal/
 
-## Known Issues
-* 
+## Blog
+### June 2022
+<p>
+Currently, I am using the Leaflet Library to render the map, and then using those pre-built objects to link to my back-end.
+</p>
+<p>
+The back-end consists of objects that take input data from the user and use it to put pins on the map and calculate other useful data.
+</p>
--- script.js
+++ script.js
@@ -13,9 +13,7 @@ const zoomLevel = 15;
 class Workout{
 
     constructor(coords, distance, duration){
-        this._date = new Date();
-        const rand = Math.trunc(Math.random() * 100 + 1);
-        this._id = (new Date.now() + '').slice(-10) + rand; //gives unique number
+        this._id = Math.trunc(Math.random() * 1000000 + 1);
 
         this._coords = coords;
         this._distance = distance;
@@ -63,11 +61,12 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-class App{
+class App {
     #map;
     #mapEvent;
+    #workouts = [];
 
-    constructor(){
+    constructor() {
         this._getPosition();
 
         form.addEventListener('submit', this._newWorkout.bind(this));
@@ -83,53 +82,57 @@ class App{
             );
     }
 
-    _loadMap(position){
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-            const URL = `https://www.google.com/maps/@${latitude},${longitude}`;
-            const coords = [latitude, longitude];
-            this.#map = L.map('map').setView(coords, zoomLevel);
+    _loadMap(position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+        const URL = `https://www.google.com/maps/@${latitude},${longitude}`;
+        const coords = [latitude, longitude];
+        this.#map = L.map('map').setView(coords, zoomLevel);
 
-            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(this.#map);
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.#map);
 
-            L.marker(coords)
-                .addTo(this.#map)
-                .bindPopup('Your location!')
-                .openPopup();
-            this.#map.on('click', this._showForm.bind(this));
+        L.marker(coords)
+            .addTo(this.#map)
+            .bindPopup('Your location!')
+            .openPopup();
+        this.#map.on('click', this._showForm.bind(this));
 
     }
 
-    _showForm(mapE){
+    _showForm(mapE) {
         this.#mapEvent = mapE;
         form.classList.remove('hidden');
         inputDistance.focus();
         document.querySelector(".begin-text").classList.add("hidden");
     }
 
-    _toggleElevationField(){
+    _toggleElevationField() {
 
     }
 
     _newWorkout(e) {
+
         const validInputs = (...inputs) => inputs.every(inp => Number.isFinite(inp));
-        const allPositive = (...inputs) => inputs.every(inp => {return inp >= 0;})
+        const allPositive = (...inputs) => inputs.every(inp => {
+            return inp >= 0;
+        })
 
         e.preventDefault();
 
         //get data from form
+        const {lat, lng} = this.#mapEvent.latlng;
         const type = inputType.value;
         const distance = +inputDistance.value;
         const duration = +inputDuration.value;
-
+        let workout;
 
         //check if data is valid
 
         // if activity __, then make the right object
-        if (type === 'running'){
+        if (type === 'running') {
             const cadence = +inputCadence.value;
             //check if data is valid
             if (!validInputs(distance, duration, cadence) ||
@@ -137,16 +140,20 @@ class App{
                 alert('inputs must be positive numbers');
                 return;
             }
-        }
-        else if (type === 'cycling'){
+
+            workout = new Running([lat, lng], distance, duration, cadence);
+
+        } else if (type === 'cycling') {
             const elevation = +inputElevation;
             if (!validInputs(distance, duration, elevation) ||
                 !allPositive(distance, duration)) {
                 alert('inputs must be positive numbers');
                 return;
             }
+            workout = new Cycling([lat, lng], distance, duration, elevation);
         }
-        // add new object to workout array
+        // add new object to work out array
+        this.#workouts.push(workout);
 
         //render workout on map as marker
 
@@ -157,19 +164,22 @@ class App{
         inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
 
         //display marker
-        const {lat, lng} = this.#mapEvent.latlng;
+
         const currCoords = [lat, lng];
         L.marker(currCoords).addTo(this.#map).bindPopup(L.popup({
             maxWidth: 250,
             minWidth: 100,
             autoClose: false,
             // closeOnClick: false,
-            className: `normal-walk-popup`,
+            // className: `normal-walk-popup`,
+            className: `${type}-popup}`
         }))
             .setPopupContent('New Popup')
             .openPopup();
     }
+
 }
+
 
 function clickError(){
     alert("Could not get position");
